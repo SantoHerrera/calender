9993a8515ad5e3b1f77a81aaa56cead548a8eb64
Rafael Herrera <santoElias65537@gmail.com>
2020-06-12 18:48:03 -0600

    displayes dates exactly how i wanted it too

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,12 @@
     <table>
       <tbody>
         <tr v-for="(row,i) in calender" :key="i">
-          <td
-            v-for="(cell, j) in row"
-            :key="`${j}-${i}`"
-            :id="`${j}-${i}`"
-            v-on:click="cellClick"
-            v-on:click.right="markCell"
-          >{{ cell.screen }}</td>
+          <td v-for="(cell, j) in row" :key="`${j}-${i}`" :id="`${j}-${i}`">{{ cell.screen }}</td>
         </tr>
       </tbody>
     </table>
 
-    <button v-on:click="returnNext2Weeks"></button>
+    <button v-on:click="displayDates"></button>
   </div>
 </template>
 
@@ -23,7 +17,8 @@ export default {
   name: "App",
   data: function() {
     return {
-      calender: this.createNestedArray(7, 3)
+      calender: this.createNestedArray(7, 3),
+      dayIndex: 4
     };
   },
   methods: {
@@ -46,29 +41,68 @@ export default {
     returnNext2Weeks: function() {
       Date.prototype.addDays = function(days) {
         var dat = new Date(this.valueOf());
+        // console.log(dat)
         dat.setDate(dat.getDate() + days);
+
+        // console.log(dat)
         return dat;
       };
 
       // create the array
       let dates = [];
 
+      let days = [];
       // define the interval of your dates
       var currentDate = new Date();
+      // let day = currentDate.getUTCDate();
+      // console.log(day);
 
       // create a loop between the interval
       while (dates.length < 14) {
+        days.push(currentDate.getDate());
         // add one day
+        dates.push(currentDate);
         currentDate = currentDate.addDays(1);
 
         // add on array
-        dates.push(currentDate);
       }
+      return days;
+    },
+    displayDates: function() {
+      // let next2Weeks = this.returnNext2Weeks()
 
-      console.log(dates)
+      let currentDate = new Date();
+      let y = currentDate.getDay()
+      let x = 0;
+      // let idk = currentDate.getDate();
+
+
+
+      // console.log(typeof y )
+
+      let next2WeeksDate = this.returnNext2Weeks();
+
+      console.log(typeof y, y)
+      // this.calender[x][startHere].screen = "p.easefs"
+
+      // this.calender[x][y].screen = `${idk}`
+
+      // console.log(next2WeeksDate[0], "why undefined")
+      for (let i = 0; i < next2WeeksDate.length; i++) {
+        if (!this.calender[x][y]) {
+          x++;
+          y = 0;
+          continue;
+        }
+        this.calender[x][y].screen = `${next2WeeksDate[i]}`;
+        y++;
+      }
+      // console.log(this.returnNext2Weeks())
     }
   }
 };
+
+// this.returnNext2Weeks()
 </script>
 
 <style>
